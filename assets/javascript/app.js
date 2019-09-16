462f90f4febb3b7d66c276c9bc8b579d8f0f4b2a
Randall <rfields1128@yahoo.com>
2019-09-16 18:15:20 -0500

    js, adding in functionality to questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+var quizEl = $("#quiz")
+var resultsEl = $("#results")
+var submitButton = $("#submit")
+var correctAnswer
+
+
+function quizBuilder(){
+    var output = []
+
+    myQuestions.forEach
+}
+
+function showResults(){
+    console.log("works")
+}
+
+quizBuilder();
+
+submitButton.on("click", function(){
+    showResults();
+});
+
+var myQuestions = [
+    {
+      question: "When was the first cup of coffee made?",
+      answers: {
+        a: "800 A.D.",
+        b: "1840",
+        c: "2019"
+      },
+      correctAnswer: "a"
+    }
+  ];
+
+
+  
